c68d33aea417bc9a8e0ed12dfb1c2918d518c214
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-05 14:22:03 +0100

    chart view

--- src/ba/unsa/etf/rpr/projekat/Controller.java
+++ src/ba/unsa/etf/rpr/projekat/Controller.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.BorderPane;
+
 public class Controller {
+
+
+    @FXML
+    BorderPane borderPane;
+
+    @FXML
+    public void handleChart(ActionEvent event){
+        CategoryAxis categoryAxisX = new CategoryAxis();
+        categoryAxisX.setLabel("Time");
+
+        NumberAxis categoryAxisY = new NumberAxis();
+        categoryAxisY.setLabel("Weight");
+
+        AreaChart areaChart = new AreaChart(categoryAxisX,categoryAxisY);
+
+        XYChart.Series data = new XYChart.Series();
+        data.setName("Progress");
+
+        data.getData().add(new XYChart.Data("1.1.2023", 80));
+        data.getData().add(new XYChart.Data("2.1.2023", 78));
+        data.getData().add(new XYChart.Data("3.1.2023", 82));
+        data.getData().add(new XYChart.Data("4.1.2023", 75));
+        data.getData().add(new XYChart.Data("5.1.2023", 78));
+        data.getData().add(new XYChart.Data("6.1.2023", 74.5));
+        data.getData().add(new XYChart.Data("7.1.2023", 76.2));
+        data.getData().add(new XYChart.Data("8.1.2023", 71));
+        data.getData().add(new XYChart.Data("9.1.2023", 62));
+        data.getData().add(new XYChart.Data("10.1.2023", 67));
+        data.getData().add(new XYChart.Data("11.1.2023", 68));
+        data.getData().add(new XYChart.Data("12.1.2023", 60));
+
+        areaChart.getData().add(data);
+        borderPane.setCenter(areaChart);
+    }
 }
--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/addWeight.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/chart.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
