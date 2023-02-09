6cf9bf7239cddc7441bf1da17f251b9a47bf44a4
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-10 00:30:27 +0100

    unit tests

---
+++ test/ba/unsa/etf/rpr/projekat/UserTest.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.projekat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getUserName() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getWeights() {
+    }
+}
