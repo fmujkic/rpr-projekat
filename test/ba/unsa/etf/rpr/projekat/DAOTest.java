4fe3b1c16fb31cb6f3fc38f27fc1a293d788c585
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-10 00:02:04 +0100

    unit tests

---
+++ test/ba/unsa/etf/rpr/projekat/DAOTest.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.projekat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DAOTest {
+
+    @org.junit.jupiter.api.Test
+    void getInstance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWeightsForUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addWeightForUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loginRegisterUser() {
+    }
+}
