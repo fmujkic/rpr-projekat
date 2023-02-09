2c2321d5707182c8a77da0dd0377874439cec133
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-10 00:21:23 +0100

    unit tests

---
+++ test/ba/unsa/etf/rpr/projekat/ControllerTest.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.projekat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void handleChart() {
+    }
+
+    @Test
+    void handleList() {
+    }
+
+    @Test
+    void handleAddWeight() {
+    }
+
+    @Test
+    void handleSubmitWeight() {
+    }
+
+    @Test
+    void login() {
+    }
+}
--- test/ba/unsa/etf/rpr/projekat/DAOTest.java
+++ test/ba/unsa/etf/rpr/projekat/DAOTest.java
@@ -3,6 +3,9 @@ package ba.unsa.etf.rpr.projekat;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 class DAOTest {
 
     @Test
@@ -13,18 +16,31 @@ class DAOTest {
 
     @Test
     void getUser() {
-
+        int userID = DAO.getInstance().loginRegisterUser("ime", "password");
+        User user = DAO.getInstance().getUser(userID);
+        Assertions.assertEquals(user.getUserName(),"ime");
     }
 
     @Test
-    void getWeightsForUser() {
+    void getWeightsForUser() throws SQLException {
+        int userID = DAO.getInstance().loginRegisterUser("ime", "password");
+        DAO.getInstance().addWeightForUser(userID, new Weight ("12.12.2012.", 123));
+        ArrayList<Weight> weights = DAO.getInstance().getWeightsForUser(userID);
+        Assertions.assertEquals(weights.get(0).getWeight(),123);
     }
 
     @Test
-    void addWeightForUser() {
+    void addWeightForUser() throws SQLException {
+        int userID = DAO.getInstance().loginRegisterUser("ime", "password");
+        DAO.getInstance().addWeightForUser(userID, new Weight ("12.12.2012.", 123));
+        ArrayList<Weight> weights = DAO.getInstance().getWeightsForUser(userID);
+        Assertions.assertEquals(weights.get(0).getDate(),"12.12.2012.");
     }
 
     @Test
     void loginRegisterUser() {
+        int userID = DAO.getInstance().loginRegisterUser("ime", "password");
+        Assertions.assertEquals(userID,DAO.getInstance().loginRegisterUser("ime", "password"));
+
     }
 }
