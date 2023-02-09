c3f0a378cd344b80943f2582cb10d909df485431
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-10 00:37:28 +0100

    unit tests

--- test/ba/unsa/etf/rpr/projekat/WeightTest.java
+++ test/ba/unsa/etf/rpr/projekat/WeightTest.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.projekat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +9,13 @@ class WeightTest {
 
     @Test
     void getWeight() {
+        Weight weight = new Weight();
+        Assertions.assertEquals(weight.getWeight(), 80);
     }
 
     @Test
     void testToString() {
+        Weight weight = new Weight();
+        Assertions.assertEquals(weight.toString(),"01012020  -  80.0");
     }
 }
