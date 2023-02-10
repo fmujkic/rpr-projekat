9aeb6f6617b1150da3bff396e6e79ea1dad23335
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-10 07:19:18 +0100

    Initial JavaDoc comments

--- src/ba/unsa/etf/rpr/projekat/DAO.java
+++ src/ba/unsa/etf/rpr/projekat/DAO.java
@@ -91,7 +91,7 @@ public class DAO {
      * Private method to extract user from result set.
      * @param rs ResultSet is given parameter.
      * @return returns Instance of an User object.
-     * @throws SQLException if there is SQL issue.
+     * @throws SQLException of there is SQL issue throws SQLException .
      */
     private User getUserFromResultSet(ResultSet rs) throws SQLException {
         User u = new User(rs.getInt(1), rs.getString(2), rs.getString(3), null);
