cc108396bd23d6b993750806876d6b7b2b94ad70
Fejsal Mujkic <fmujkic2@etf.unsa.bam>
2023-02-10 07:57:01 +0100

    JavaDoc

--- src/ba/unsa/etf/rpr/projekat/User.java
+++ src/ba/unsa/etf/rpr/projekat/User.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * Creating DTO for user.
+ */
 public class User implements Serializable {
     private int ID;
     private String userName, password;
